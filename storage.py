8a7a4b129116a476bbe98eaa8b1b431658821693
Sergey Postnikov <ser.post2017@gmail.com>
2022-10-13 18:09:47 +0300

    get_records restructured

--- storage.py
+++ storage.py
@@ -2,5 +2,21 @@ import pandas
 
 
 def get_records():
-    excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
-    return excel_data_df.to_dict(orient='records')
+    goods = {}
+    excel_data_df = pandas.read_excel(
+        'wine.xlsx', 
+        sheet_name='Лист1',
+        na_values=None, 
+        keep_default_na=False)
+
+    for product in excel_data_df.to_dict(orient='records'):
+        if product['Категория'] in goods:
+            goods[product['Категория']].append(product)
+        else:
+            goods[product['Категория']] = [product]
+    return goods
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(get_records())
