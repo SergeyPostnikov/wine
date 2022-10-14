f0179c5d5dadcbfc818a7a2adfe269ebc530ad78
Sergey Postnikov <ser.post2017@gmail.com>
2022-10-14 17:48:02 +0300

    implemented taking .env argument for get_records

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 from storage import get_records
-
+import os
+from dotenv import load_dotenv
+import argparse
 
 def get_noun(years):
     if 11 <= years % 100 <= 19:
@@ -16,24 +18,29 @@ def get_noun(years):
         return "лет"
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('storage', help='storage location')
+    args = parser.parse_args()
+    print(args.name)
 
-template = env.get_template('template.html')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
+    template = env.get_template('template.html')
 
+    delta_years = datetime.now().year - 1920
+    rendered_page = template.render(
+        winery_age=delta_years, 
+        noun=get_noun(delta_years),
+        records=get_records(os.getenv("PATH_TO_STORAGE"), default='wine.xlsx') 
+    )
 
-delta_years = datetime.now().year - 1920
-rendered_page = template.render(
-    winery_age=delta_years, 
-    noun=get_noun(delta_years),
-    records=get_records() 
-)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 5000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 5000), SimpleHTTPRequestHandler)
+    server.serve_forever()
